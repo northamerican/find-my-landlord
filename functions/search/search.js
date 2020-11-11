ae1260f286ef9dbad5ee19f12f891a7413981f22
Chris Bitsakis <c.bitsakis@gmail.com>
2020-11-11 13:18:30 -0500

    Update search.js

--- functions/search/search.js
+++ functions/search/search.js
@@ -1,43 +1,43 @@
-const fs = require('fs');
-const { searchIndex } = JSON.parse(fs.readFileSync(require.resolve('./assets/search-index.json')))
+// const fs = require('fs');
+// const { searchIndex } = JSON.parse(fs.readFileSync(require.resolve('./assets/search-index.json')))
 
-const FlexSearch = require('./assets/flexsearch.js')
+// const FlexSearch = require('./assets/flexsearch.js')
 
-const headers = {
-  'Access-Control-Allow-Origin': '*'
-}
+// const headers = {
+//   'Access-Control-Allow-Origin': '*'
+// }
 
-const searchResultsLimit = 50
-const propertyIndexColumn = 'hash'
+// const searchResultsLimit = 50
+// const propertyIndexColumn = 'hash'
 
 exports.handler = async ({ body }) => {
-  const jsonBody = JSON.parse(body)
-  const { query } = jsonBody
+  // const jsonBody = JSON.parse(body)
+  // const { query } = jsonBody
 
-  // console.log({ jsonBody, query })
-  // console.log(searchIndex.length)
+  // // console.log({ jsonBody, query })
+  // // console.log(searchIndex.length)
 
-  const flex = new FlexSearch({
-    encode: "simple",
-    profile: "fast",
-    doc: {
-      id: propertyIndexColumn,
-      field: ["search"]
-    }
-  })
+  // const flex = new FlexSearch({
+  //   encode: "simple",
+  //   profile: "fast",
+  //   doc: {
+  //     id: propertyIndexColumn,
+  //     field: ["search"]
+  //   }
+  // })
 
-  flex.add(searchIndex)
+  // flex.add(searchIndex)
 
-  const results = flex.search(query, searchResultsLimit + 1)
-  const response = {
-    results
-  }
+  // const results = flex.search(query, searchResultsLimit + 1)
+  // const response = {
+  //   results
+  // }
 
   // console.log({ response, results })
 
   return {
     statusCode: 200,
-    body: JSON.stringify(response),
+    body: JSON.stringify({ results: [] }),
     headers: JSON.stringify(headers)
   }
 }
